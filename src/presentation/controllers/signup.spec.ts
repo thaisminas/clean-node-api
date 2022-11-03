60f9b6816accce43996ae34a43f971d78d6dcf77
Thais <thaisminasds@gmail.com>
2022-11-02 22:07:07 -0300

    feat: ensure signUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,9 @@ import {SignUpController} from './signup'
 
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', ()=> {
+        //instanciar a classe
         const sut = new SignUpController()
+        //mock do objeto a ser passado para meu metodo
         const httpRequest = {
             body: {
                 name: 'any_name',
@@ -14,5 +16,7 @@ describe('SignUp Controller', () => {
 
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
+        //espera que o meu body seja igual ao erro.
+        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
     })
 })
